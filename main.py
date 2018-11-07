9fb36f44ae337d61c530a0941e8baa2b9f1f7b81
granulfo <gabrielranulfo14@gmail.com>
2018-11-07 14:20:52 -0200

    update function export

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from tkinter import *
 import sqlite3
 from datetime import *
-def cadastro () :
+
+
+def cadastro():
     janela = Toplevel()
     janela.title("Cadastro")
+
     def get():
         if muda.get() == 1:
             return 1
@@ -11,27 +14,30 @@ def cadastro () :
             print("0")
             return 0
         return None
+
     muda = IntVar()
     lb_carreteiro = Checkbutton(janela, text="text", variable=muda, command=get)
     lb_carreteiro.place(x=153, y=230)
+
     def get_data():
-        nome_func = str (nome.get())
+        nome_func = str(nome.get())
         dtNascto_func = str(dtNascto.get())
         cpf_func = str(cpf.get())
         nis_func = str(nis.get())
         codCBO_func = str(codCBO.get())
         codCateg_func = str(codCateg.get())
-        carreteiro_func = str (get())
-        vrremun_func = str (wd_vrremun.get())
+        carreteiro_func = str(get())
+        vrremun_func = str(wd_vrremun.get())
         conn = sqlite3.connect('autonomos.db')
         cursor = conn.cursor()
         cursor.execute("""
-        INSERT INTO autonomos (nome, dtnscto, cpf, nis, codcbo, codcateg, carreteiro, vrremun)
+        INSERT INTO autonomos (nome, dtnscto, cpf, nis, codcbo, codcateg, vrremun, carreteiro)
         VALUES (?,?,?,?,?,?,?,?)
-        """, (nome_func, dtNascto_func, cpf_func, nis_func, codCBO_func, codCateg_func, carreteiro_func, vrremun_func))
+        """, (nome_func, dtNascto_func, cpf_func, nis_func, codCBO_func, codCateg_func, vrremun_func, carreteiro_func))
         conn.commit()
         print('Dados inseridos com sucesso.')
         conn.close()
+
     nome = Entry(janela)
     nome.place(x=100, y=50, width=420)
     lb_nome = Label(janela, text="Nome :")
@@ -59,25 +65,27 @@ def cadastro () :
     carreteiro = Label(janela, text="Carreteiro / Frete :")
     carreteiro.place(x=10, y=230)
     vrremun = Label(janela, text='Valor da Remuneração :')
-    vrremun.place(x=10,y=260)
+    vrremun.place(x=10, y=260)
     wd_vrremun = Entry(janela)
-    wd_vrremun.place(x=160,y=260,width=100)
+    wd_vrremun.place(x=160, y=260, width=100)
     bt_salvar = Button(janela, text='Salvar', width=10, command=get_data)
     bt_salvar.place(x=410, y=255)
     janela.geometry("530x300+100+100")
     janela.focus_force()
     janela.grab_set()
     janela.mainloop()
-def export_xml () :
+
+
+def export_xml():
     def cod():
         from xml.dom.minidom import Document
         conn = sqlite3.connect('autonomos.db')
         cursor = conn.cursor()
         id = int(wd_id.get())
-        print(cursor.execute("select * from autonomos where id = ?;",(id,)))
+        print(cursor.execute("select * from autonomos where id = ?;", (id,)))
         exporta_id = cursor.fetchall()
         exportado = (exporta_id[0][0:9])
-        carreta = float (exportado[8])
+        carreta = float(exportado[7])
         # document xml create
         doc = Document()
         # elements
@@ -109,20 +117,18 @@ def export_xml () :
         codLotacao = doc.createElement('codLotacao')
         remunPerApur = doc.createElement('remunPerApur')
 
-
-#itens remun
+        # itens remun
         ideTabRubr = doc.createElement('ideTabRubr')
         ideTabRubr2 = doc.createElement('ideTabRubr')
 
         qtdRubr = doc.createElement('qtdRubr')
         qtdRubr2 = doc.createElement('qtdRubr')
 
-
         vrRubr = doc.createElement('vrRubr')
         vrRubr2 = doc.createElement('vrRubr')
         vrRubr3 = doc.createElement('vrRubr')
 
-        codRubr  = doc.createElement('codRubr')
+        codRubr = doc.createElement('codRubr')
         codRubr2 = doc.createElement('codRubr')
         codRubr3 = doc.createElement('codRubr')
 
@@ -130,24 +136,8 @@ def export_xml () :
         itensRemun2 = doc.createElement('itensRemun')
         itensRemun3 = doc.createElement('itensRemun')
 
-
         infoComplCont = doc.createElement('infoComplCont')
         codCBO = doc.createElement('codCBO')
-        '''				
-                        <itensRemun>
-							<codRubr>440</codRubr>
-							<ideTabRubr>1</ideTabRubr>fixo em 1
-							<qtdRubr>11.00</qtdRubr>fixo 11%
-							<vrRubr>30.90</vrRubr>valor não pode ser maior que 621,04 se passar deve setar o campo neste valor
-						</itensRemun>
-						<itensRemun>
-							<codRubr>442</codRubr>
-							<ideTabRubr>1</ideTabRubr>
-							<vrRubr>280.90</vrRubr>
-						</itensRemun>
-		'''
-
-
 
         # child
         doc.appendChild(root)
@@ -179,7 +169,7 @@ def export_xml () :
         ideEstabLot.appendChild(remunPerApur)
         remunPerApur.appendChild(itensRemun)
         remunPerApur.appendChild(itensRemun2)
-        if carreta == "1" :
+        if carreta == "1":
             remunPerApur.appendChild(itensRemun3)
 
         itensRemun.appendChild(codRubr)
@@ -191,19 +181,8 @@ def export_xml () :
         itensRemun2.appendChild(ideTabRubr2)
         itensRemun2.appendChild(vrRubr2)
 
-        '''if carreta == "1" :
-            itensRemun.appendChild(codRubr)
-            itensRemun.appendChild(ideTabRubr)
-            itensRemun.appendChild(vrRubr)
-            
-            
-            
-            '''
-
         dmDev.appendChild(infoComplCont)
         infoComplCont.appendChild(codCBO)
-
-
 
         # receive data
 
@@ -211,32 +190,32 @@ def export_xml () :
 
             id = "ID1"
             hora = str(datetime.now().strftime("%Y%m%d%H%M%S"))
-            id = (id + str(cpf)+ "00" + hora + "00001")
+            id = (id + str(cpf) + "00" + hora + "00001")
             return id
+
         ident = gera_id(exportado[3])
 
-        a = str(1)#indretif
-        b = str(1)#indapuracao
+        a = str(1)  # indretif
+        b = str(1)  # indapuracao
         data_atual = date.today()
         c = data_atual.strftime('%Y-%m')
-        d = str(1)#tpamb
-        e = str(1)#procEmi
-        f = str("1.0")#verProc
-        g = str(1)#tpinsc
-        h = str("03582844")#nrinsc
-        i = str(exportado[3])#cpf
-        j = str(exportado[4])#nis
-        k = str(exportado[1])#nome
-        nascimento =str(exportado[2])
-        data = datetime.strptime(nascimento,"%Y%m%d").date()
-        l = str (data)#DTNASCTO
-        m = str(1)#idedmdev
-        n = str(exportado[6])#codcateg
-        o = str("1")#tpinsc
-        p = str("03582844000186")#nrisnc
-        q = str("C01S000003")#codLotacao
-        r = str(exportado[5])#codcbo
-        rubricas = [442,440,3400,443,446,447]
+        d = str(1)  # tpamb
+        e = str(1)  # procEmi
+        f = str("1.0")  # verProc
+        g = str(1)  # tpinsc
+        h = str("03582844")  # nrinsc
+        i = str(exportado[3])  # cpf
+        j = str(exportado[4])  # nis
+        k = str(exportado[1])  # nome
+        nascimento = str(exportado[2])
+        data = datetime.strptime(nascimento, "%Y%m%d").date()
+        l = str(data)  # DTNASCTO
+        m = str(1)  # idedmdev
+        n = str(exportado[6])  # codcateg
+        o = str("1")  # tpinsc
+        p = str("03582844000186")  # nrisnc
+        q = str("C01S000003")  # codLotacao
+        r = str(exportado[5])  # codcbo
 
         # atributes
         root.setAttribute('xmlns', 'http://www.esocial.gov.br/schema/evt/evtRemun/v02_04_02')
@@ -279,37 +258,37 @@ def export_xml () :
         txt25 = doc.createTextNode(r)
         codCBO.appendChild(txt25)
 
-        #s = str(rubricas[0])
-        #t = str(remuneracao)
         carreta = exportado[7]
-        remuneracao = float (exportado[8])
-
-
+        remuneracao = float(exportado[8])
 
         def remun(remuneracao, carreta):
             if carreta == "1":
-                remuneracao2 = remuneracao * 0.20
+                remuneracao2 = float(({}).format(remuneracao * 0.20))
                 remuneracao1 = remuneracao2 * 0.11
                 return remuneracao, remuneracao2, remuneracao1
             elif carreta == "0":
-                remuneracao2 = remuneracao * 0.20
+
+                remuneracao * 0.11
+                remuneracao2 = ("{:.2f}".format(remuneracao * 0.11))
+                remuneracao = ("{:.2f}".format(remuneracao))
+
                 return remuneracao, remuneracao2
             else:
                 print("ERRO")
 
-        if carreta == "0" :
-            teste = remun(remuneracao,carreta)
+        if carreta == "0":
+            teste = remun(remuneracao, carreta)
+
             print(teste)
             dd = str(teste[0])
             ddd = str(teste[1])
-        else :
+        else:
             teste = remun(remuneracao, carreta)
             print(teste)
             dd = str(teste[0])
+            dd = str(teste[0])
             ddd = str(teste[1])
             dddd = str(teste[2])
-
-
 
         txt29 = doc.createTextNode("440")
         codRubr.appendChild(txt29)
@@ -332,26 +311,29 @@ def export_xml () :
         txt32 = doc.createTextNode(dd)
         vrRubr2.appendChild(txt32)
 
-        nomedoxml = ident + ".xml"
+        nomedoxml = ("01_01_s-1200_") + ident[16:24] + "_" + ident[24:30] + ".xml"
         doc.writexml(open(nomedoxml, 'w'),
                      addindent='    ',
                      newl='\n')
         doc.unlink()
         print("Exportado com Sucesso")
-    export = Toplevel ()
+
+    export = Toplevel()
     a = Button(export, text="Exportar", command=cod)
-    a.place(x=250,y=55)
+    a.place(x=250, y=55)
     lb_nis = Label(export, text="ID : ")
     lb_nis.place(x=100, y=60)
-    wd_id = Entry(export,width=10)
-    wd_id.place(x=150,y=60)
+    wd_id = Entry(export, width=10)
+    wd_id.place(x=150, y=60)
     export.geometry("530x300+100+100")
     export.mainloop()
-def alterar () :
-    def alterar_data () :
+
+
+def alterar():
+    def alterar_data():
         def get_id2():
             id = wd_id.get()
-            nome = str (novo_nome.get())
+            nome = str(novo_nome.get())
             nascto = novo_dtNascto.get()
             cpf = novo_cpf.get()
             nis = novo_nis.get()
@@ -365,24 +347,26 @@ def alterar () :
             UPDATE autonomos
             SET nome = ?, dtnscto = ?, cpf = ?, nis = ?, codcbo = ?, codcateg = ?, carreteiro = ?, vrremun = ?
             WHERE id = ?
-            """, (nome, nascto, cpf, nis, cbo, categoria, remuneracao, carreteiro, id))
+            """, (nome, nascto, cpf, nis, cbo, categoria, carreteiro, remuneracao, id))
 
             conn.commit()
 
             print('Dados atualizados com sucesso.')
 
             conn.close()
+
         def get2():
             if muda2.get() == 1:
                 return "1"
             else:
                 return "0"
+
         janela = Toplevel()
         janela.title("Alterar")
         wd_id = Entry(janela)
-        wd_id.place(x=100,y=20)
-        lb_wdid = Label(janela,text="ID :")
-        lb_wdid.place(x=10,y=20)
+        wd_id.place(x=100, y=20)
+        lb_wdid = Label(janela, text="ID :")
+        lb_wdid.place(x=10, y=20)
         novo_nome = Entry(janela)
         novo_nome.place(x=100, y=50, width=420)
         lb_nome = Label(janela, text="Nome :")
@@ -423,27 +407,30 @@ def alterar () :
         janela.focus_force()  #
         janela.grab_set()  #
         janela.mainloop()
+
     conn = sqlite3.connect('autonomos.db')
     cursor = conn.cursor()
-    alterar = Toplevel ()
+    alterar = Toplevel()
     alterar.geometry("530x300+100+100")
-    lbemails = Listbox(alterar,width=60)
+    lbemails = Listbox(alterar, width=60)
     lbemails.place(x=25, y=40)
     cursor.execute("""
     SELECT * FROM autonomos;
     """)
     for linha in cursor.fetchall():
-        lbemails.insert(END,linha)
+        lbemails.insert(END, linha)
     conn.close()
-    lblMat = Label(alterar,text="Escolha o Item Abaixo :")
-    lblMat.place(x=200,y=20)
-    lbemails = Button(alterar,text="Próximo >>",command=alterar_data)
+    lblMat = Label(alterar, text="Escolha o Item Abaixo :")
+    lblMat.place(x=200, y=20)
+    lbemails = Button(alterar, text="Próximo >>", command=alterar_data)
     lbemails.place(x=409, y=235)
     alterar.mainloop()
 
-def excluir () :
-    excluir = Toplevel ()
-    def get_id () :
+
+def excluir():
+    excluir = Toplevel()
+
+    def get_id():
         id = wd_id.get()
         conn = sqlite3.connect('autonomos.db')
         cursor = conn.cursor()
@@ -454,25 +441,30 @@ def excluir () :
         conn.commit()
         print('Dados atualizados com sucesso.')
         conn.close()
+
     wd_id = Entry(excluir)
     wd_id.place(x=100, y=20)
     lb_wdid = Label(excluir, text="ID :")
     lb_wdid.place(x=10, y=20)
     a = Button(excluir, text="Excluir", command=get_id)
-    a.place(x=250,y=55)
+    a.place(x=250, y=55)
     excluir.geometry("530x300+100+100")
     excluir.mainloop()
-def menu () :
+
+
+def menu():
     menu = Tk()
     menu.title("Programa")
     bt = Button(menu, text="Novo", width=50, command=cadastro)
-    bt.place(x=50,y=100)
+    bt.place(x=50, y=100)
     bt = Button(menu, text="Alterar", width=50, command=alterar)
-    bt.place(x=50,y=130)
+    bt.place(x=50, y=130)
     bt = Button(menu, text="Excluir", width=50, command=excluir)
-    bt.place(x=50,y=160)
+    bt.place(x=50, y=160)
     bt = Button(menu, text="Exportar para XML", width=50, command=export_xml)
-    bt.place(x=50,y=190)
+    bt.place(x=50, y=190)
     menu.geometry("530x300+100+100")
     menu.mainloop()
+
+
 menu()
